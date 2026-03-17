e0746cdc051fb94d33499411f2dc16e967edbefb
Salasar Services <digitalmarketing@salasarservices.co.in>
2026-03-17 11:23:38 +0530

    Add custom button color styling in main.py

--- main.py
+++ main.py
@@ -498,6 +498,14 @@ div[role="tooltip"] * {
   color: #ffffff !important;
 }
 
+/*Custom Button Color*/
+const buttonElement = document.querySelector('button.st-emotion-cache-9114l4.etjibo42');
+if (buttonElement) {
+  await setElementStyles(buttonElement, {
+    backgroundColor: 'blue',
+    color: 'white'
+  });
+}
 </style>
 """,
     unsafe_allow_html=True,
